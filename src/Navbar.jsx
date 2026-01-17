8bffbc262049ea9a7ebbdb39e13b9de991310c81
aayanA91 <aayan.practical@gmail.com>
2026-01-17 17:14:19 +0500

    React navbar update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-const Navbar = () =>{
-
-const navlinks = [
-{name : "Home" , path : "/"},
-{name : "About" , path : "/about"},
-{name : "Contactus" , path : "/contactus"},
-]
-return(
-    <>
-    <div>
-        <ul>
-        {navlinks.map((items)=>(
-            <li key={items.path}>
-                <a href={items.path}>{items.name}</a>
+const Navbar = () => {
+  const navlinks = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Contact Us", path: "/contactus" },
+  ];
+  return (
+    <header className="header">
+      <div className="logo">space for logo</div>
+      <nav className="nav">
+        <ul className="nav-links">
+          {navlinks.map((items) => (
+            <li key={items.path} className="nav-item">
+              <a href={items.path}>{items.name}</a>
             </li>
-        ))}
+          ))}
         </ul>
-    </div>
-    </>
-)
-}
+      </nav>
+      <div className="auth-buttons">
+        <button className="auth-login">login</button>
+        <button className="auth-signup">signup</button>
+      </div>
+    </header>
+  );
+};
 export default Navbar;
